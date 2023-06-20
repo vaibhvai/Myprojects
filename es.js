ea8a8470a9e013c80d0d30da6d1dff81bcc1bdf1
vaibhvai <vaibhavimore74@gmail.com>
2023-06-20 11:19:45 +0530

    add msg

---
+++ es.js
@@ -0,0 +1,10 @@
+const  squareofNumber= (number)=> number*number;
+console.log(squareofNumber(10));
+
+const isEven = number =>{
+    if (number%2===0){
+        return true
+    }
+    return false
+}
+console.log(isEven(100));
